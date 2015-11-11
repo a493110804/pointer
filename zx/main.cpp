094b40e7a02f826b3808ec20a3d5cdbb9dffccd2
a493110804 <493110804@qq.com>
2015-11-11 18:15:22 +0800

    Initial Commit

---
+++ zx/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  zx
+//
+//  Created by 魏新宇 on 15/11/11.
+//  Copyright © 2015年 魏新宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
